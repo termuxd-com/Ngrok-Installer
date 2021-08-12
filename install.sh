c6b4d4c9354f44781537b1f9d36afc794c32a125
0xberkay <0xberkay@gmail.com>
2021-08-12 14:34:43 +0300

    update

--- install.sh
+++ install.sh
@@ -1,20 +1,15 @@
 #!/bin/dash
 echo "\e[31mWait for download"
 sleep 3
-apt upgrade -y
-pkg update -y
-apt install -y wget
+apt update && apt upgrade -y
+
+
 clear
-
-
-
-wget "https://termuxd.com/files/files.zip" -O ngrok.zip
-unzip ngrok.zip
-cat ngrok > /data/data/com.termux/files/usr/bin/ngrok
-chmod 700 /data/data/com.termux/files/usr/bin/ngrok
-rm ngrok ngrok.zip
+chmod 777 *
+rm -rf /data/data/com.termux/files/usr/bin/ngrok
+mv -v ngrok /data/data/com.termux/files/usr/bin
 clear
 
 echo "\e[92mNgrok downloaded successfully"
-ngrok
+
 
